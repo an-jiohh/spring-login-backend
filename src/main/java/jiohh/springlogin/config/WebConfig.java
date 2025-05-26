d858f90ff7fb4dec0599020cb8651be5c3ecde52
an-jiohh <dkswlgh99@naver.com>
2025-05-27 01:22:03 +0900

    refactor : DDD 아키텍처 적용을 위해 패키지 변경
    - User 도메인에 따른 기존 구조 및 패키지 개선

--- src/main/java/jiohh/springlogin/config/WebConfig.java
+++ src/main/java/jiohh/springlogin/config/WebConfig.java
@@ -1,6 +1,6 @@
 package jiohh.springlogin.config;
 
-import jiohh.springlogin.interceptor.LogInterceptor;
+import jiohh.springlogin.config.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
--- src/main/java/jiohh/springlogin/config/interceptor/LogInterceptor.java
+++ src/main/java/jiohh/springlogin/config/interceptor/LogInterceptor.java
@@ -1,4 +1,4 @@
-package jiohh.springlogin.interceptor;
+package jiohh.springlogin.config.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
